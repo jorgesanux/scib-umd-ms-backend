11e322451e2fa49965432a7feb9912d0763cf530
Jorge Sanabria <jorgesanux1@gmail.com>
2022-04-10 20:39:37 -0500

    Agreagadas rutas de GETUser

--- services/administration/routes/user.js
+++ services/administration/routes/user.js
@@ -1,22 +1,32 @@
 import express from "express";
 
 import UserController from "../controller/userController.js";
+import APIError from "../util/apiError.js";
+import APIResponse from "../util/apiResponse.js";
 
 const router = express.Router();
 const userController = new UserController();
 
 router.get('/', async function(req, res, next) {
   res.json(
-    await userController.getAllUsers()
+    new APIResponse({
+      statusCode: 200,
+      results: await userController.getAllUsers()
+    })
   );
 });
 
 router.get("/:userId", async function(req, res, next){
   const user = await userController.getUser(req.params.userId);
-  res.statusCode = user === null ? 404 : 200;
-  res.json(
-    await userController.getUser(req.params.userId)
-  );
+  if(user === null){
+    res.status(404);
+    res.json(new APIError(404, `No se encontró un usuario con el id ${req.params.userId}`).toJSON());
+    return ;
+  }
+  res.json(new APIResponse({
+    statusCode: 200,
+    result: user
+  }).toJSON());
 });
 
 export default router;
