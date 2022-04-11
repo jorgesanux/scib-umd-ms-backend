dfd8cfcae4f0548c0bcd9ff53f1465d7be7678bb
Jorge Sanabria <jorgesanux1@gmail.com>
2022-04-10 20:39:21 -0500

    Agregada clase  de APIError

--- services/administration/util/apiError.js
+++ services/administration/util/apiError.js
@@ -1,5 +1,18 @@
 class APIError extends Error{
+    constructor(statusCode, message){
+        super(message);
+        this.name = this.constructor.name;
+        this.status = statusCode;
+    }
 
+    toJSON(){
+        return {
+            status: this.status,
+            message: this.message,
+            result: null,
+            results: []
+        };
+    }
 }
 
 export default APIError;
